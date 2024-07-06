868b5bcb8b9827073f8cee4d91043f77093dcbcc
Tsiareshka, Matsvei <matsvei.tsiareshka@study.thws.de>
2024-07-06 14:25:33 +0200

    ein zahk nach server schicken

--- API/api.py
+++ API/api.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/send_value', methods=['POST'])
 def send_value():
     data = request.get_json()
-    value = data.get('noteManuale')
+    value = data.get('command')
     timestamp = time.time()
     print(f"Received value: {value} at {timestamp}")
     return jsonify({'status': 'success', 'timestamp': timestamp}), 200
